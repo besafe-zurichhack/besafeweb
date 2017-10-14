9d6b8e7e8db690187475fabf71a1ce26b854a967
Marcos Pérez <marcos13594@gmail.com>
2017-10-14 19:06:41 +0200

    init

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import UserProfile from '@/components/UserProfile'
+import UserList from '@/components/UsersList'
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/dash',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: UserList
+    },
+    {
+      path: '/users/:id',
+      name: 'userProfile',
+      component: UserProfile
+    }
+  ]
+})
